93ecaf34c4f588e1ef86dfc5ecb445212836c96e
Enthalpy Change <enthalpychange@users.noreply.github.com>
2020-03-02 21:29:34 -0800

    add abstract base models

---
+++ tan/core/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from tan.identity.models import Identity
+
+
+class TimeStampedModel(models.Model):
+    """Abstract base class model for created and modified timestamps.
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Process(TimeStampedModel):
+    """Abstract base class model for processes.
+    """
+    class Statuses(models.TextChoices):
+        SUBMITTED = 'SM', 'Submitted'
+        ASSIGNED = 'AS', 'Assigned'
+        RESOLVED = 'RS', 'Resolved'
+        CLOSED = 'CL', 'Closed',
+        REOPENED = 'RO', 'Reopened'
+
+    name = models.CharField(max_length=255)
+    creator = models.ForeignKey(Identity, on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=2,
+        choices=Statuses.choices,
+        default=Statuses.SUBMITTED,
+    )
+
+    class Meta:
+        abstract = True
