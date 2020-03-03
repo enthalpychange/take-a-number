34b9e274cc0a95191d10e674d3ae0732d7638d88
Enthalpy Change <enthalpychange@users.noreply.github.com>
2020-03-02 19:56:03 -0800

    change from sqlite to postgres

--- config/settings/base.py
+++ config/settings/base.py
@@ -60,17 +60,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 AUTH_USER_MODEL = 'identity.Identity'
 
 # Password validation
--- config/settings/local.py
+++ config/settings/local.py
@@ -8,3 +8,19 @@ from .base import * # noqa
 SECRET_KEY = 'secret key for local development only!'
 
 DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+#
+# docker run -d -e "POSTGRES_PASSWORD=postgres" -p 127.0.0.1:5432:5432 postgres
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '',
+    }
+}
