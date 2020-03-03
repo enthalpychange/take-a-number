656143b6501122ed07d41bcf340b7dbdf7b63aaf
Enthalpy Change <enthalpychange@users.noreply.github.com>
2020-03-02 19:53:41 -0800

    add identity to INSTALLED_APPS and change user model

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
+    'tan.identity.apps.IdentityConfig',
     'tan.tracker.apps.TrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -70,6 +71,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'identity.Identity'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
