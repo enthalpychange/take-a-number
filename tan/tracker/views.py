5a5c2bb88116ac27bc27d82434d1fccc151c258d
Enthalpy Change <enthalpychange@users.noreply.github.com>
2020-03-13 19:19:40 -0700

    add success message

--- tan/tracker/views.py
+++ tan/tracker/views.py
@@ -19,13 +19,24 @@ from .models import (
 class IncidentCreateView(LoginRequiredMixin, BSModalCreateView):
     model = Incident
     form_class = IncidentCreateForm
-    success_message = 'IT IS FINE'
+    success_message = 'Your ticket number is %(ticket_number)s. You will be contacted soon.'
     success_url = reverse_lazy('tracker:index')
 
     # https://docs.djangoproject.com/en/dev/topics/class-based-views/generic-editing/#models-and-request-user
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
+
+    # Add ticket number to success message
+    # https://docs.djangoproject.com/en/3.0/ref/contrib/messages/#adding-messages-in-class-based-views
+    def get_success_message(self, cleaned_data):
+        # Django calls this method twice. The first time self.object.id is None
+        if self.object.id:
+            incident_id = self.object.id
+            # Pad with zeros
+            return self.success_message % {'ticket_number': f'{incident_id:07}'}
+        else:
+            return 'Success!'
 
 
 class IncidentDetailView(LoginRequiredMixin, DetailView):
