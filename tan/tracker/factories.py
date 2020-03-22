403c012415534a593f011d2da93a21ff48bc4acd
Enthalpy Change <enthalpychange@users.noreply.github.com>
2020-03-22 10:18:39 -0700

    update factory to use lorem ipsum

--- tan/tracker/factories.py
+++ tan/tracker/factories.py
@@ -4,7 +4,7 @@ from faker import Faker
 from tan.identity.models import Identity
 from tan.tracker.models import Incident
 
-fake = Faker()
+fake = Faker(locale=['la', 'en_US'])
 fake.seed_instance(1)
 users = Identity.objects.all()
 
@@ -14,7 +14,7 @@ def generate_description(obj):
     """
     s = fake.random_int(min=3, max=12)
     p = fake.random_int(min=1, max=5)
-    return '\n'.join([fake.paragraph(nb_sentences=s, variable_nb_sentences=True) for n in range(p)])
+    return '\n'.join([fake['la'].paragraph(nb_sentences=s, variable_nb_sentences=True) for n in range(p)])
 
 
 def random_user(obj):
@@ -25,6 +25,6 @@ class IncidentFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Incident
 
-    name = factory.Faker('sentence', nb_words=12, variable_nb_words=True)
+    name = factory.LazyAttribute(lambda o: fake['la'].sentence(nb_words=10))
     description = factory.LazyAttribute(generate_description)
     creator = factory.LazyAttribute(random_user)
